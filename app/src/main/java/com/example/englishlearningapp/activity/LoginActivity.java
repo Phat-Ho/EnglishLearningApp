adddd2e6403dd7036ef014c6c2946aff05630a46
Đức Tài <ronin32014@gmail.com>
2020-02-27 21:44:44 +0700

    Register Screen

--- app/src/main/java/com/example/englishlearningapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/englishlearningapp/activity/LoginActivity.java
@@ -2,15 +2,32 @@ package com.example.englishlearningapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.englishlearningapp.R;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton materialButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        MappingView();
+        materialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void MappingView() {
+        materialButton = findViewById(R.id.login_next_button);
     }
 }
