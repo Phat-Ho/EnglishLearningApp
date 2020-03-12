2cd6926add64fe48968d5887b2a4b1c5c7228c29
Phat Ho <hoangphatho@gmail.com>
2020-03-12 22:45:30 +0700

    Update tinh nang tra tu

--- app/src/main/java/com/example/englishlearningapp/navigation_bottom_fragments/SearchFragment.java
+++ app/src/main/java/com/example/englishlearningapp/navigation_bottom_fragments/SearchFragment.java
@@ -2,6 +2,7 @@ package com.example.englishlearningapp.navigation_bottom_fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +15,22 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.englishlearningapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.content.Context.INPUT_METHOD_SERVICE;
 
@@ -67,6 +83,7 @@ public class SearchFragment extends Fragment {
 
     EditText edtSearch;
     TextView txtViewTranslatedWord;
+    String url = "https://translate.yandex.net/api/v1.5/tr.json/translate";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -78,7 +95,7 @@ public class SearchFragment extends Fragment {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    txtViewTranslatedWord.setText(edtSearch.getText().toString());
+                    getTranslatedWord();
                     hideSoftKeyBoard();
                     return true;
                 }
@@ -86,6 +103,46 @@ public class SearchFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private void getTranslatedWord(){
+        final String key = "trnsl.1.1.20200311T182743Z.a0f2a10aa284e86b.503d45acafbe1fe322301fb49be3039cafbb1fa5";
+        final String text = edtSearch.getText().toString().trim();
+        final String lang = "vi";
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray textArray = jsonObject.getJSONArray("text");
+                            for (int i = 0; i < textArray.length(); i++){
+                                stringBuilder.append(textArray.getString(i) + "\n");
+                            }
+                            txtViewTranslatedWord.setText(stringBuilder.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("AAA", error.toString());
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("key", key);
+                params.put("text", text);
+                params.put("lang", lang);
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
     private void hideSoftKeyBoard() {
