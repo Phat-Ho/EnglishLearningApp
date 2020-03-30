71b8fe10386835fe859cd00d62f75f86b7cecc0c
hoangphat258 <hoangphatho@gmail.com>
2020-03-30 10:39:58 +0700

    Update AlarmReceiver

--- app/src/main/java/com/example/englishlearningapp/receiver/AlarmReceiver.java
+++ app/src/main/java/com/example/englishlearningapp/receiver/AlarmReceiver.java
@@ -1,11 +1,13 @@
 package com.example.englishlearningapp.receiver;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -25,6 +27,8 @@ public class AlarmReceiver extends BroadcastReceiver {
     NotificationCompat.Builder notiBuilder;
     public ArrayList<Word> historyWords;
     DatabaseAccess db;
+    public static final String NOTIFICATION_CHANNEL_ID = "4655";
+    public static final String NOTIFICATION_CHANNEL_NAME = "my_channel";
     @Override
     public void onReceive(Context context, Intent intent) {
         //Get history words array index to show notification
@@ -40,6 +44,17 @@ public class AlarmReceiver extends BroadcastReceiver {
         String html = historyWords.get(arrayIndex).getHtml();
         String word = historyWords.get(arrayIndex).getWord();
 
+        //Implement notification channel
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
         //Start Meaning activity when click on notification
         Intent meaningIntent = new Intent(context, MeaningActivity.class);
         meaningIntent.putExtra("html", html);
@@ -47,7 +62,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         PendingIntent meaningPendingIntent = PendingIntent.getActivity(context, 0, meaningIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         //Build notification
-        notiBuilder = new NotificationCompat.Builder(context).setContentTitle(word)
+        notiBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID).setContentTitle(word)
                                                                 .setContentIntent(meaningPendingIntent)
                                                                 .setSmallIcon(R.mipmap.ic_launcher);
         notificationManager.notify((int) System.currentTimeMillis(), notiBuilder.build());
