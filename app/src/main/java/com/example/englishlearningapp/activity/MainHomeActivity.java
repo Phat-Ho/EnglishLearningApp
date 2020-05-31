e92fca341358ce3a4fd0dada7a41ef9f9149e767
hoangphat258 <hoangphatho@gmail.com>
2020-05-31 20:47:19 +0700

    Update Camera

--- app/src/main/java/com/example/englishlearningapp/activity/MainHomeActivity.java
+++ app/src/main/java/com/example/englishlearningapp/activity/MainHomeActivity.java
@@ -107,8 +107,10 @@ public class MainHomeActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        Bitmap takenImage = BitmapFactory.decodeFile(HomeGridViewAdapter.photoFile.getAbsolutePath());
                         try {
+
+                            Bitmap takenImage = BitmapFactory.decodeFile(HomeGridViewAdapter.photoFile.getAbsolutePath());
+
                             rotateImage(takenImage);
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -128,9 +130,10 @@ public class MainHomeActivity extends AppCompatActivity {
     private void passBitmapIntent(Bitmap takenImage){
         try {
             //Write file
-            String filename = "bitmap.png";
+            String filename = "bitmap.jpeg";
             FileOutputStream stream = this.openFileOutput(filename, Context.MODE_PRIVATE);
-            takenImage.compress(Bitmap.CompressFormat.PNG, 10, stream);
+            takenImage.compress(Bitmap.CompressFormat.JPEG, 20, stream);
+
 
             //Cleanup
             stream.close();
@@ -164,41 +167,6 @@ public class MainHomeActivity extends AppCompatActivity {
         passBitmapIntent(rotatedBitmap);
     }
 
-    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-
-    public static Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filePath, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(filePath, options);
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
