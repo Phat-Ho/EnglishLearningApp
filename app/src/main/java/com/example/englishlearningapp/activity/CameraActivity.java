b56df0ef7437ac845a2dd7fc35b0642eb7310fe4
hoangphat258 <hoangphatho@gmail.com>
2020-05-31 17:15:23 +0700

    Update camera

--- app/src/main/java/com/example/englishlearningapp/activity/CameraActivity.java
+++ app/src/main/java/com/example/englishlearningapp/activity/CameraActivity.java
@@ -32,7 +32,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.englishlearningapp.R;
+import com.example.englishlearningapp.adapters.HomeGridViewAdapter;
 import com.example.englishlearningapp.fragments.HistoryFragment;
 import com.example.englishlearningapp.models.Word;
 import com.example.englishlearningapp.utils.CustomTextRecognizer;
@@ -42,7 +44,9 @@ import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -100,12 +104,49 @@ public class CameraActivity extends AppCompatActivity {
         try {
             FileInputStream is = this.openFileInput(filename);
             bmp = BitmapFactory.decodeStream(is);
-            imgTranslate.setImageBitmap(bmp);
             is.close();
+            imgTranslate.setImageBitmap(bmp);
+//            Picasso.get().load(filename).resize(50, 50).centerCrop().into(imgTranslate);
             getTextFromImage(bmp);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
     }
 
     private void getTextFromImage(Bitmap bitmap){
--- app/src/main/java/com/example/englishlearningapp/activity/MainHomeActivity.java
+++ app/src/main/java/com/example/englishlearningapp/activity/MainHomeActivity.java
@@ -103,14 +103,22 @@ public class MainHomeActivity extends AppCompatActivity {
         if (requestCode == HomeGridViewAdapter.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(HomeGridViewAdapter.photoFile.getAbsolutePath());
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Bitmap takenImage = BitmapFactory.decodeFile(HomeGridViewAdapter.photoFile.getAbsolutePath());
+                        try {
+                            rotateImage(takenImage);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
                 // RESIZE BITMAP, see section below
 //                passBitmapIntent(takenImage);
-                try {
-                    rotateImage(takenImage);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
             } else { // Result was a failure
 //                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
@@ -122,7 +130,7 @@ public class MainHomeActivity extends AppCompatActivity {
             //Write file
             String filename = "bitmap.png";
             FileOutputStream stream = this.openFileOutput(filename, Context.MODE_PRIVATE);
-            takenImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            takenImage.compress(Bitmap.CompressFormat.PNG, 10, stream);
 
             //Cleanup
             stream.close();
@@ -154,6 +162,42 @@ public class MainHomeActivity extends AppCompatActivity {
         }
         Bitmap rotatedBitmap = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
         passBitmapIntent(rotatedBitmap);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
     }
 
     @Override
