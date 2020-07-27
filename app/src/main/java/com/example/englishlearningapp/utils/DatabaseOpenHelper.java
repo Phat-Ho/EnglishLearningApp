aa61477db73c1aa197b531893b03f6be4420a912
ductai98 <ronin32014@gmail.com>
2020-07-27 19:59:08 +0700

    fix bug

--- app/src/main/java/com/example/englishlearningapp/utils/DatabaseOpenHelper.java
+++ app/src/main/java/com/example/englishlearningapp/utils/DatabaseOpenHelper.java
@@ -8,7 +8,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "dict_hh.db";
-    private static final int DATABASE_VERSION = 7;
+    private static final int DATABASE_VERSION = 11;
     private static final String TAG = "DatabaseOpenHelper";
     public final Context context;
 
